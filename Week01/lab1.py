e4282eec243f32fdc058d3d9fa9d780475957d84
AustinS06 <austinsamms06@gmail.com>
2026-01-16 16:41:42 -0500

    Update lab1.py

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -3,3 +3,13 @@
 
 # Question 2: Defining an Array (list)
 my_array = [1, 4, 7, 9]
+
+#Question 3:
+
+#Question 4:
+
+#Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtered by age:", userAge)
